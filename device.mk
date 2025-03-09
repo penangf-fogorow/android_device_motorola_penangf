527d56966c23ea496511b5fe1b491e9dac500d2c
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-03-09 14:29:36 +0200

    penangf: build required NFC interfaces

--- device.mk
+++ device.mk
@@ -7,5 +7,11 @@
 # Inherit from common device makefile.
 $(call inherit-product, device/motorola/mt6768-common/device.mk)
 
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc_snxxx@1.2-service \
+    android.hardware.secure_element_snxxx@1.2-service \
+    libchrome.vendor 
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/motorola/penangf/penangf-vendor.mk)
