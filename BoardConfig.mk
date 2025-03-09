91ab7761353db863ad52518db5e36e1fdc671b85
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-03-09 03:41:55 +0200

    penangf: define modules flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,21 @@ DEVICE_PATH := device/motorola/penangf
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := penangf
 
+## vendor_boot modules
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD += $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor_boot))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(addprefix $(KERNEL_PATH)/penangf/vendor_boot-modules/, $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD))
+
+## recovery modules
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+RECOVERY_MODULES += $(addprefix $(KERNEL_PATH)/penangf/vendor_boot-modules/, $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD))
+
+## Prevent duplicated entries (to solve duplicated build rules problem)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(sort $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES) $(RECOVERY_MODULES))
+
+## vendor modules
+BOARD_VENDOR_KERNEL_MODULES_LOAD += $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor))
+BOARD_VENDOR_KERNEL_MODULES += $(wildcard $(KERNEL_PATH)/penangf/vendor-modules/*.ko)
+
 # inherit from common mt6768-common
 -include device/motorola/mt6768-common/BoardConfigCommon.mk
 
